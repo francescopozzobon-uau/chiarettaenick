c1b6c5e248c029e7c46f611aaf6ad49cee020766
Nicolò Rosato <nicolorosato@192.168.1.59>
2026-04-21 22:14:32 +0200

    Caching

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+export const fetchCache = "force-no-store";
+
 export const metadata: Metadata = {
   title: "Chiara & Nicolò",
   description: "Matrimonio di Chiara e Nicolò",
