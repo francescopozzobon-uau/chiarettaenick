a8465b2a6e020b75ec0b90b8ec6fcc0133611bf4
Nicolò Rosato <nicolorosato@mac.lan>
2026-04-22 09:06:39 +0200

    Thank you page

---
+++ app/grazie/page.tsx
@@ -0,0 +1,19 @@
+export default function GraziePage() {
+  return (
+    <main
+      style={{
+        display: "flex",
+        minHeight: "100vh",
+        alignItems: "center",
+        justifyContent: "center",
+        textAlign: "center",
+      }}
+    >
+      <p>
+        Grazie per aver contribuito.
+        <br />
+        Ci vediamo il 13 giugno per festeggiare insieme!
+      </p>
+    </main>
+  );
+}
